c36acf410014f3e9db58ec4a794fe1035c5c6f3a
hanseglz <glzhang23@gmail.com>
2021-11-03 17:10:44 -0400

    trigger pytest

---
+++ backend/test_api.py
@@ -0,0 +1,19 @@
+import json
+import unittest
+import sys
+import requests
+import pytest
+
+sys.path.append(".")
+
+from data.connection import connect
+from data import connection
+
+class Api(unittest.TestCase):
+    def setUp(self):
+        self.db = connect()
+        self.connection = connection
+
+    def test_hello(self):
+        response = requests.get('http://localhost:5000')
+        self.assertEqual(response.status_code)
