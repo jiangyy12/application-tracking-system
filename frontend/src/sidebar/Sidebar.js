5232722799d55b6940ace31025369baea3c76380
Minghao <41974269+MinghoKwok@users.noreply.github.com>
2021-11-03 20:47:25 -0400

    Update Sidebar.js

--- frontend/src/sidebar/Sidebar.js
+++ frontend/src/sidebar/Sidebar.js
@@ -23,9 +23,6 @@ export default class Sidebar extends Component {
                     <div onClick={() => this.props.switchPage('SchoolPage')}>
                         <i className="fas fa-sort-alpha-down left-nav-icon"></i>
                     </div>
-                    <div onClick={() => this.props.switchPage('NotePage')}>
-                        <i className="fas fa-sort-alpha-asc left-nav-icon"></i>
-                    </div>
                 </div>
             </div>
         );
