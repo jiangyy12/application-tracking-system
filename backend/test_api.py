34c9fb7efa40367d0011b1f8606f509c1f990c91
yliang123 <89490528+yliang123@users.noreply.github.com>
2021-11-04 21:39:25 -0400

    Update test_api.py

--- backend/test_api.py
+++ backend/test_api.py
@@ -15,5 +15,5 @@ class Api(unittest.TestCase):
         self.connection = connection
 
     def test_hello(self):
-        response = requests.get('http://127.0.0.1:5000')
+        response = requests.get('http://localhost:5000')
         self.assertEqual(response.status_code, 200)
