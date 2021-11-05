9439e2b20ee78d1410b76067cd48ff2cc015d6a0
hanseglz <glzhang23@gmail.com>
2021-11-04 22:39:33 -0400

    update update with heroku & fix testing cases

--- backend/test_api.py
+++ backend/test_api.py
@@ -1,6 +1,7 @@
 import json
 import unittest
 import sys
+import app
 import requests
 import pytest
 
@@ -10,10 +11,23 @@ from data.connection import connect
 from data import connection
 
 class Api(unittest.TestCase):
+
+    # def setUp(self):
+    #     self.db = connect()
+    #     self.connection = connection
+    #
+    # def test_hello(self):
+    #     response = requests.get('http://localhost:5000')
+    #     self.assertEqual(response.status_code, 200)
+
     def setUp(self):
-        self.db = connect()
-        self.connection = connection
+        self.app = app.app.test_client()
+        self.app.testing = True
 
     def test_hello(self):
-        response = requests.get('http://localhost:5000')
-        self.assertEqual(response.status_code, 200)
+        home = self.app.get('/')
+        self.assertIn('Hello World!', str(home.data))
+
+
+if __name__ == "__main__":
+    unittest.main()
